468829732e4dff279c18dbe7a7d2a54f455ebfdc
Steven Schmitt <stevenschmitt7@web.de>
2024-11-04 11:29:02 +0100

    Small changes

--- ios-project/view/RecipeInfoView.swift
+++ ios-project/view/RecipeInfoView.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct RecipeInfoView: View {
+    
+    let recipe: Recipe
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("\(recipe.title)")
     }
 }
 
 #Preview {
-    RecipeInfoView()
+    var recipe = Recipe.recipes[99]
+    RecipeInfoView(recipe: recipe)
 }

97016bd64348d28f54f9fdf95e5dcf30c77cba5b
Steven Schmitt <stevenschmitt7@web.de>
2024-11-04 11:27:12 +0100

    Added view for a recipe

---
+++ ios-project/view/RecipeInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeInfoView.swift
+//  ios-project
+//
+//  Created by Poimandres on 03.11.24.
+//
+
+import SwiftUI
+
+struct RecipeInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeInfoView()
+}

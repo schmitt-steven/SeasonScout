7cc524e41aba4d4bd8bc47df3fcc75384ca37d52
Steven Schmitt <82405062+schmitt-steven@users.noreply.github.com>
2024-11-18 11:43:25 +0100

    RecipeInfoView: Added similar recipes, ingredient list, imrpoved pie chart

---
+++ ios-project/Views/Recipe/SimilarRecipesView.swift
@@ -0,0 +1,18 @@
+//
+//  SimilarRecipesView.swift
+//  ios-project
+//
+//  Created by Poimandres on 14.11.24.
+//
+
+import SwiftUI
+
+struct SimilarRecipesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimilarRecipesView()
+}
